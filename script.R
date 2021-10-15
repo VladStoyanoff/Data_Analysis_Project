03514c6d9f38f0275f29134bbb047a8b151933a9
Vladimir Stoyanov <88051591+VladStoyanoff@users.noreply.github.com>
2021-10-15 17:37:28 +0300

    Update script.R

--- script.R
+++ script.R
@@ -182,7 +182,9 @@ all_trips_v2 %>%
   
   # STEP 5: EXPORT SUMMARY FILE FOR FURTHER ANALYSIS IF NEEDED
   
-  
+  counts <- aggregate(all_trips_v2$ride_length ~ all_trips_v2$member_casual + all_trips_v2$day_of_week, FUN = mean)
+write.csv(counts, file = '~/Desktop/Divvy_Exercise/avg_ride_length.csv')
+
 
 
 
